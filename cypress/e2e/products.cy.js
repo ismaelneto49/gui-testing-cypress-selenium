dc7d7756dc83d497906c1ee684c9eab164032ef5
ismael.neto <ismaelneto49@gmail.com>
2024-09-14 19:46:16 -0300

    chore: add tests

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -6,7 +6,7 @@ describe('products', () => {
     cy.get('.primary').click();
   });
   // Remove .only and implement other test cases!
-  it('details is listing all variants', () => {
+  it.skip('details is listing all variants', () => {
     // Click in products in side menu
     cy.clickInFirst('a[href="/admin/products/"]');
     // Type in value input to search for specify product
@@ -14,7 +14,7 @@ describe('products', () => {
     // Click in filter blue button
     cy.get('*[class^="ui blue labeled icon button"]').click();
     // Click in details of the remain product
-    cy.clickInFirst('*[class^="ui labeled icon button "]');
+    cy.clickInFirst('*[class^="ui labeled icon button"]');
 
     // Assert that details page is listing all variants
     cy.get('body')
@@ -24,7 +24,7 @@ describe('products', () => {
       .and('contain', '000F_office_grey_jeans-variant-3')
       .and('contain', '000F_office_grey_jeans-variant-4');
   });
-  it.only('changes the name of a product', () => {
+  it.skip('changes the name of a product', () => {
     cy.clickInFirst('a[href="/admin/products/"]');
     cy.get('[class="icon pencil"]').first().click();
     cy.get('[id="sylius_product_translations_en_US_name"]').clear().type('000F jeans cinza de escritório');
@@ -33,14 +33,36 @@ describe('products', () => {
 
     cy.get('body').should('contain', '000F jeans cinza de escritório');
   });
-  it('test case 3', () => {
+  it.skip('deletes a product', () => {
     cy.clickInFirst('a[href="/admin/products/"]');
+    cy.get('[id="criteria_search_value"]').type('000F_office_grey_jeans');
+    cy.get('[class="ui blue labeled icon button"]').click();
+    cy.get('[class="icon trash"]').first().scrollIntoView().click({ force: true });
+
+    cy.get('body').should('contain', 'Products have been successfully deleted.');
   });
-  it('test case 3', () => {
+  it.skip("edits an item's variant", () => {
     cy.clickInFirst('a[href="/admin/products/"]');
+    cy.get('[class="ui labeled icon button "]').first().click();
+    cy.get('[class="ui labeled icon button edit-variant"]').first().scrollIntoView().click();
+    cy.wait(1000);
+    cy.get('[id="sylius_product_variant_translations_en_US_name"]').clear().type('SAPATO');
+    cy.get('[id="sylius_save_changes_button"]').click();
+
+    cy.get('body').should('contain', 'SAPATO');
   });
-  it('test case 3', () => {
+  it("deletes an item's variant", () => {
+    cy.viewport(1920, 1080)
     cy.clickInFirst('a[href="/admin/products/"]');
+    cy.get('[class="ui labeled icon button "]').first().click();
+    cy.get('[class="ui labeled icon button edit-variant"]').first().scrollIntoView().click();
+    cy.wait(1000);
+    cy.get('[id="sylius_save_changes_button"]').click();
+    cy.get('[class="icon trash"]').first().scrollIntoView().click({ force: true });
+    cy.wait(100)
+    cy.get('[id="confirmation-button"]').click()
+
+    cy.get('body').should('contain', 'Product_variants have been successfully deleted.');
   });
   it('test case 3', () => {
     cy.clickInFirst('a[href="/admin/products/"]');
